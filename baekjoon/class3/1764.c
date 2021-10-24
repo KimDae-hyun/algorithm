1a26175055ae9fecaa6de1db9cde2d65104ef9a0
KimDae-hyun <dhkim5608@naver.com>
2021-10-24 13:37:30 +0900

    Feat : algorithm solved
    - 백준 알고리즘 풀이 : 1764

--- baekjoon/class3/1764.c
+++ baekjoon/class3/1764.c
@@ -11,10 +11,10 @@ int cmp(const void *a, const void *b)
 
 int main(void)
 {
-    char **listen;
-    char **sort;
-    int n;
-    int m;
+    char **listen;// 모든 문자열을 받아줄 배열
+    char **sort; // 듣도 보도 못한 문자열
+    int n; // 듣도 못한 사람 수
+    int m; // 보도 못한 사람 수
     int i;
     int j;
 
@@ -22,33 +22,37 @@ int main(void)
     listen = (char **)calloc(n + m + 1, sizeof(char *));
     sort = (char **)calloc(m + 1, sizeof(char *));
     i = -1;
-    while (++i < n + m)
+    while (++i < n + m) // 듣도 보도 못한 모든 사람
     {
         listen[i] = (char *)calloc(21, sizeof(char));
-        if (i < m)
-            sort[i] = (char *)calloc(21, sizeof(char));
         scanf("%s", listen[i]);
     }
-    qsort(listen, n + m, sizeof(char *), cmp);
+    qsort(listen, n + m, sizeof(char *), cmp); // 정렬
     i = -1;
     j = 0;
     while (++i < n + m - 1)
+    {
         if (strcmp(listen[i], listen[i + 1]) == 0)
+        { //같다면 듣도 보도 못한 사람
+            sort[j] = (char *)calloc(21, sizeof(char));
             sort[j++] = listen[i];
-    printf("%d\n", j);
+        }
+    }
+    printf("%d\n", j); // 듣보잡 수 출력
     qsort(sort, j, sizeof(char *), cmp);
     i = -1;
     while (++i < j)
-        printf("%s\n", sort[i]);
-    i = -1;
+        printf("%s\n", sort[i]);// 듣보잡 명단 출력
+/*    i = -1;
     while (++i < n + m)
     {
         free(listen[i]);
-        if (i < m)
+        if (i < j)
             free(sort[i]);
     }
-    free(listen);
     free(sort);
+    free(listen);*/
     return (0);
 }
 
+//gcc에서는 이중프리 에러가 없지만, 백준에서 이중프리로 런타임 에러 발생
