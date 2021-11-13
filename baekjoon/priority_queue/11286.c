5cd468f4b6ec1c75dda6392d1adfa00644783ea8
KimDae-hyun <dhkim5608@naver.com>
2021-11-13 14:31:51 +0900

    Feat : priority_queue
    - 최대힙, 최소힙 풀이
    - 절댓값 힙 실패 : 출력에서 문제가 발생한 것으로 예상하는 중

---
+++ baekjoon/priority_queue/11286.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void insert(int *heap, int x, int j)
+{
+    while ((j != 1) && abs(heap[j / 2]) >= abs(x)) // 입력된 절댓값이 자식 노드보다 작으면
+    {
+        if (abs(heap[j / 2]) == abs(x) && heap[j / 2] <= x) // 절댓값은 같으나 부호의 차이가 있을 수 있다.
+            break ;
+        heap[j] = abs(heap[j / 2]); // 자식 노드를 더 아래 자식 노드로 내린다.
+        j /= 2;
+    }
+    heap[j] = x; // 부모노드보다 크고, 자식노드보다 작은 위치의 노드에 입력.
+}
+
+void delete(int *heap, int j)
+{
+    int first;
+    int tmp;
+    int parent;
+    int child;
+
+    first = heap[1]; // 우선 순위가 가장 높은 값 = 출력될 값
+    tmp = heap[--j]; // 가장 마지막에 입력 된 값
+    parent = 1;
+    child = 2;
+    while (child <= j)
+    {
+        if ((child < j) && abs(heap[child]) > abs(heap[child + 1])) // 왼쪽 및 오른쪽 자식 노드를 비교 하여 더 작은 자식 노드 구하기
+            child++;
+        if (abs(tmp) <= abs(heap[child])) // 마지막 값과 동일 숫자 => 부모를 제외하고 현재 자식 노드부터 끝까지 모두 크거나 같은 숫자 
+            break ;
+        
+        heap[parent] = heap[child]; // 부모위치에 자식노드 입력
+        parent = child; 
+        child *= 2; 
+    }
+    heap[parent] = tmp; // 마지막 인덱스 위치 값 수정
+    heap[j] = 0; // 마지막 위치 0 초기화
+    printf("%d\n", first);
+}
+
+int main(void)
+{
+    int *heap; // 힙 = 완전 이진트리, 최소힙 구현
+    int n; // 입력 개수
+    int x; // 입력 값
+    int i; // 입력 카운트
+    int j; // 힙 인덱스
+
+    scanf("%d", &n);
+    heap = (int *)calloc(n + 1, sizeof(int));
+    i = 0;
+    j = 1;
+    while (++i <= n)
+    {
+        scanf("%d", &x);
+        if (x == 0) // 입력 값이 0이면 출력
+        {
+            delete(heap, j); // 최소 값 출력 및 제거
+            if (j > 1) // 인덱스 --
+                j--;
+        }
+        else
+        {
+            insert(heap, x, j); // 값 입력
+            j++; // 인덱스 ++
+        }
+    }
+    return (0);
+}
